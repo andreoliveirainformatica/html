aca13cb5f2125133ca8aa32ab610ff1d21bdce67
Andre de Oliveira <andreoliveirainformatica@users.noreply.github.com>
2022-03-11 15:08:03 -0300

    Add files via upload

---
+++ html.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+func titulo(urls ...string) <-chan string {
+	c := make(chan string)
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			a := r.FindStringSubmatch(string(html))
+			if len(a) > 0 {
+				c <- r.FindStringSubmatch(string(html))[1]
+			} else {
+				c <- fmt.Sprintf("Title não enconrado -> url %s", url)
+			}
+		}(url)
+	}
+	return c
+}
